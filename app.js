278bd8d95b3a56f81348892fdc7b5f97cf99625f
tim wu <timwuuk@gmail.com>
2020-01-12 09:19:34 +0000

    prettier ignore, url params and much more

--- app.js
+++ app.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const ejs = require("ejs");
+const _ = require("lodash");
 
 const homeStartingContent =
   "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
@@ -9,6 +10,7 @@ const contactContent =
   "Scelerisque eleifend donec pretium vulputate sapien. Rhoncus urna neque viverra justo nec ultrices. Arcu dui vivamus arcu felis bibendum. Consectetur adipiscing elit duis tristique. Risus viverra adipiscing at in tellus integer feugiat. Sapien nec sagittis aliquam malesuada bibendum arcu vitae. Consequat interdum varius sit amet mattis. Iaculis nunc sed augue lacus. Interdum posuere lorem ipsum dolor sit amet consectetur adipiscing elit. Pulvinar elementum integer enim neque. Ultrices gravida dictum fusce ut placerat orci nulla. Mauris in aliquam sem fringilla ut morbi tincidunt. Tortor posuere ac ut consequat semper viverra nam libero.";
 
 const app = express();
+let posts = [];
 
 app.set("view engine", "ejs");
 
@@ -16,7 +18,7 @@ app.use(express.urlencoded({ extended: true }));
 app.use(express.static("public"));
 
 app.get("/", (req, res) => {
-  res.render("home", { textContent: homeStartingContent });
+  res.render("home", { textContent: homeStartingContent, posts: posts });
 });
 
 app.get("/about", (req, res) => {
@@ -32,7 +34,28 @@ app.get("/compose", (req, res) => {
 });
 
 app.post("/compose", (req, res) => {
-  console.log(req.body.input);
+  const post = {
+    title: req.body.postTitle,
+    body: req.body.postBody
+  };
+  posts.push(post);
+  res.redirect("/");
+});
+
+app.get("/posts/:postTitle", (req, res) => {
+  let paramTitle = _.lowerCase(req.params.postTitle);
+  // if (posts.find(p => _.lowerCase(p.title) === paramTitle)) {
+  //   console.log("found it");
+  // } else {
+  //   console.log("Not found");
+  // }
+  posts.forEach(p => {
+    if (_.lowerCase(p.title) === paramTitle) {
+      res.render("post", { title: p.title, body: p.body });
+    } else {
+      console.log("not found");
+    }
+  });
 });
 
 app.listen(3000, function() {
